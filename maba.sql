26b837f21b8a05c59b3a7343a9e3d090e270a282
adiskafajar <adiskafajar87@gmail>
2020-12-29 19:46:51 +0700

    update sql

--- maba.sql
+++ maba.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 07, 2020 at 05:43 AM
+-- Generation Time: Dec 29, 2020 at 01:44 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -45,31 +45,9 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `username`, `password`, `name`, `gender`, `address`, `contact`, `role`, `accepted`) VALUES
 (1, 'admin', 'laravue87', 'admin', 'man', 'jl.road', '082137556774', 'admin', '1'),
-(2, 'maba', 'maba123', 'mahasiswa maba baru', 'woman', 'jl.michi', '0899547363', 'maba', '1'),
-(3, 'brandal123', 'brandal123', 'brandal', 'man', 'jl. road michi', '08213543', 'maba', '0'),
-(4, 'rajin123', 'rajin123', 'rajin', 'woman', 'jl. jalan', '089777', 'maba', '1'),
-(16, 'asd', 'asd', 'bambang', 'man', 'asd', 'asd', 'maba', '0'),
-(17, 'mashiro123', 'mashiro123', 'mashiro', 'woman', 'Road. blueberry', '838383', 'maba', '1'),
-(19, 'asd', 'asd', 'hikari', 'woman', 'jl. pisang', '08134123', 'maba', '1'),
-(20, 'nana123', 'nana123', 'nana', 'woman', 'Road. blackberry', '848484', 'maba', '1'),
-(23, 'asd', 'asd', 'hikari', 'woman', 'asd', 'asd', 'maba', '0'),
-(24, 'yuna123', 'asd', 'yuuna', 'woman', 'jl. stroberry', '949494', 'maba', '0'),
 (26, 'asd', 'asd', 'korona', 'woman', 'asd', 'asd', 'maba', '?'),
-(29, 'asd', 'asd', 'asada', 'woman', 'asd', 'asd', 'maba', '1'),
-(31, 'asd', 'asd', 'hinata', 'woman', 'asd', '', 'maba', '1'),
-(32, 'asd', 'asd', 'ruru', 'woman', 'asd', 'asd', 'maba', '?'),
-(33, 'asd', 'asd', 'suya', 'woman', 'maou jo', '09302930', 'maba', '?'),
-(68, 'sukebe', 'asd', 'tamaki', 'woman', 'daihachi', '123123', 'maba', '?'),
-(69, 'asd', 'asd', 'iris', 'woman', 'asd', 'asd', 'maba', '1'),
-(70, 'asd', 'asd', 'yurina', 'man', 'asd', 'asd', 'maba', '?'),
-(71, 'touka123', 'touka123', 'touka', 'woman', 'asdasd', '123123', 'maba', '1'),
-(72, '123', '123', '123', 'woman', '123', '123', 'maba', '?'),
-(73, 'shiina123', 'asd', 'shiina', 'woman', 'asd', 'asd', 'maba', '?'),
-(74, 'aoba123', 'aoba123', 'aoba', 'woman', 'asd', '123', 'maba', '1'),
-(75, 'chino123', 'chino123', 'chino', 'woman', 'rabbit house', '123', 'maba', '1'),
-(76, 'cocoa123', 'cocoa123', 'cocoa', 'woman', 'rabbit house', '3333', 'maba', '1'),
-(77, 'maria123', 'maria123', 'maria', 'woman', 'asd12', '123123', 'maba', '?'),
-(78, 'hojou123', 'hojou123', 'hojou', 'woman', 'asd', '123', 'maba', '?');
+(81, 'maba1', 'maba1', 'maba1', 'woman', 'asdfasdfasdf', '123123', 'maba', '1'),
+(82, 'covid', 'covid', 'covid', 'woman', 'asd', '123', 'maba', '?');
 
 --
 -- Indexes for dumped tables
@@ -89,7 +67,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
